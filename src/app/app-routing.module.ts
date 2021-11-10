3cee554c7ebfc30883069a396919b3a583292e72
Nihat S <nhtsarimehmet@gmail.com>
2021-11-10 14:24:26 +0300

    yönlendirmeler yapıldı

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MoviesComponent } from './movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'movies',
+    component: MoviesComponent
+  },
+  {
+    path: 'movies/category/:categoryId',
+    component: MoviesComponent
+  },
+  {
+    path: 'movies/:movieId',
+    component: MovieDetailsComponent
+  },
 
+
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
