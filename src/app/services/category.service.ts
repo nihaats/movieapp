7a19a02a13fa2fde4ef9dad949d832c81a28f085
Nihat S <nhtsarimehmet@gmail.com>
2021-11-10 14:26:44 +0300

    category service'i oluşturuldu

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  url = "http://localhost:3000/categories";
+
+  constructor(private http: HttpClient) { }
+
+  getCategories():Observable<Category[]>{
+    return this.http.get<Category[]>(this.url);
+  }
+}
