ab5fe15a7d644c70aad8f6214917ca20cfef577f
Nihat S <nhtsarimehmet@gmail.com>
2021-11-14 23:56:07 +0300

    kategori oluşturma ekranı yönlendirmesi yapıldı

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryCreateComponent } from './category-create/category-create.component';
 import { MovieCreateComponent } from './movie-create/movie-create.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesComponent } from './movies/movies.component';
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'movies/create',
     component: MovieCreateComponent
+  },
+  {
+    path: 'category/create',
+    component: CategoryCreateComponent
   },
   {
     path: 'movies/:movieId',
